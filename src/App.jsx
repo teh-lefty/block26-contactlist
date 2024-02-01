fb3b650bd99b814e1a52921b77a7d5d95c289a0f
teh-lefty <102568631+teh-lefty@users.noreply.github.com>
2024-01-31 21:05:54 -0800

    tried adding selected contactId

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import ContactList from './components/ContactList'
 import './App.css'
+import SelectedContact from './components/SelectedContact.jsx'
 
 export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
   return (
     <>
-    
+    {selectedContactId ? (
+      <div>Selected Contact View</div>
+    ) : (
     <ContactList />
+    )}
     </>
   );
 }
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from "react";
+import { useEffect } from "react";
 import ContactRow from "./ContactRow";
+
 
 const dummyContacts = [
   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -8,9 +10,26 @@ const dummyContacts = [
   { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
 ];
 
+ContactList setSelectedContactId={setSelectedContactId}
+
 export default function ContactList() {
   const [contacts, setContacts] = useState(dummyContacts);
 console.log("Contacts: ", contacts)
+useEffect(() => {
+  async function fetchContacts() {
+    try {
+      const response = await fetch(
+        "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+      );
+      const result = await response.json();
+      setContacts(result)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  fetchContacts()
+
+}, []);
   return (
 
 <table className="contactList">
